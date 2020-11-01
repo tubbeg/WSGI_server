7e8f01754a2f9aead33530ebba326776a265796f
Tobias Gärdebro <tubbeg@gmail.com>
2020-11-01 16:08:35 +0100

    Added methods for WSGI

--- server.py
+++ server.py
@@ -2,3 +2,20 @@ import io
 from gevent import socket
 import sys
 
+
+class Server(object):
+    def __init__(self):
+        self.address_family = socket.AF_INET #IPV4
+        self.socket_type = socket.SOCK_STREAM # TCP
+        self.request_queue_size = 5 # standard queue size
+        self._app = None
+
+    def set_app(self, app):
+        self._app = app
+
+    def get_environ(self):
+        return NotImplementedError()
+
+    def start_response(self):
+        return NotImplementedError()
+
